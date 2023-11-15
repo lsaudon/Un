d1d6d49df627a9af738470de39239ba9fce5f68b
lsaudon <lsaudon@gmail.com>
2023-11-15 21:59:25 +0100

    feat(UnService.cs): Implement UnService with StartGame method

--- Un.GrpcService/Services/UnService.cs
+++ Un.GrpcService/Services/UnService.cs
@@ -1,6 +1,10 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
 namespace Un.GrpcService.Services;
 
-public class UnService
+public class UnService : Un.UnBase
 {
-  
+  public override Task<StartGameReply> StartGame(Empty request, ServerCallContext context) =>
+    Task.FromResult(new StartGameReply { GameId = Guid.NewGuid().ToString() });
 }
