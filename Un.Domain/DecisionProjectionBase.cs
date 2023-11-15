c9767b488318e6deda28740e3b72c701610f7949
lsaudon <lsaudon@gmail.com>
2023-11-15 22:40:17 +0100

    fix: add null check for evt in Apply method

--- Un.Domain/DecisionProjectionBase.cs
+++ Un.Domain/DecisionProjectionBase.cs
@@ -6,15 +6,12 @@ public abstract class DecisionProjectionBase
 
   public void Apply(IDomainEvent evt)
   {
+    ArgumentNullException.ThrowIfNull(evt);
     if (_handlersByType.TryGetValue(evt.GetType(), out Action<IDomainEvent>? apply))
     {
       apply(evt);
     }
   }
 
-  protected void AddHandler<T>(Action<T> apply)
-    where T : IDomainEvent
-  {
-    _handlersByType.Add(typeof(T), o => apply((T)o));
-  }
+  protected void AddHandler<T>(Action<T> apply) where T : IDomainEvent => _handlersByType.Add(typeof(T), o => apply((T)o));
 }
