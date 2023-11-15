e738c33e3dae9806b59b066fcaea338cdb9c272d
lsaudon <lsaudon@gmail.com>
2023-11-15 21:59:25 +0100

    feat(Program.cs): Replace GreeterService with UnService and remove comments

--- Un.GrpcService/Program.cs
+++ Un.GrpcService/Program.cs
@@ -1,19 +1,9 @@
 using Un.GrpcService.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 builder.Services.AddGrpc();
 
 WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/",
-           () =>
-             "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+app.MapGrpcService<UnService>();
+app.MapGet("/", () => string.Empty);
 app.Run();
